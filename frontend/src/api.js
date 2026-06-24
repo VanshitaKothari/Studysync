917d714e383b39cd82e061efe08201d1b68720cc
Vanshita <Kvanshita05@gmail.com>
2026-06-24 09:49:07 +0530

    Update API URL to production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: 'https://studysync-backend-si0l.onrender.com',
+});
 });
 
 API.interceptors.request.use((req) => {
